b420d7bf8be7d858453c17d8c692cd770dc20c7c
yang <yanghj@daumyangui-MacBook-Pro.local>
2015-02-22 01:42:44 +0900

    AuctionMessageTranslator test success

--- src/auctionsniper/AuctionMessageTranslator.java
+++ src/auctionsniper/AuctionMessageTranslator.java
@@ -5,7 +5,13 @@ import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
 
 public class AuctionMessageTranslator implements MessageListener {
+	private final AuctionEventListener listener;
+	
+	public AuctionMessageTranslator(AuctionEventListener listener) {
+	  this.listener = listener;
+	}
+	
 	public void processMessage(Chat chat, Message message) { 
-		
+		listener.auctionClosed();
 	}
 }
