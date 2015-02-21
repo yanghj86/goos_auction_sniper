c466b353657b866d84cdfe4501335ce833ebbfd3
yang <yanghj@daumyangui-MacBook-Pro.local>
2015-02-22 02:20:09 +0900

    eventListener Processing By Message

--- src/auctionsniper/AuctionMessageTranslator.java
+++ src/auctionsniper/AuctionMessageTranslator.java
@@ -1,5 +1,7 @@
 package auctionsniper;
 
+import java.util.HashMap;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
@@ -12,6 +14,23 @@ public class AuctionMessageTranslator implements MessageListener {
 	}
 	
 	public void processMessage(Chat chat, Message message) { 
-		listener.auctionClosed();
+		HashMap<String, String> event = unpaceEventFrom(message);
+	    
+	    String eventType = event.get("Event");
+	    if ("CLOSE".equals(eventType)) { 
+	      listener.auctionClosed(); 
+	    } if ("PRICE".equals(eventType)) { 
+	      listener.currentPrice(Integer.parseInt(event.get("CurrentPrice")), Integer.parseInt(event.get("Increment")));
+	    }
 	}
+
+	private HashMap<String, String> unpaceEventFrom(Message message) {
+		HashMap<String, String> event = new HashMap<String, String>();
+		for (String element : message.getBody().split(";")) {
+			String[] pair = element.split(":");
+			event.put(pair[0].trim(), pair[1].trim());
+		}
+		return event;
+	}
+
 }
